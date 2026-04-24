eef8c5fdd20880f11b82f97baf15f5a873546e81
Narendra Patil <narendrap5805@gmail.com>
2026-04-24 11:53:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ x = int(input("first no :" ))
 y = int(input("second no :"))
 
 print(x+y)
+
+
+n = int(input("Input size"))
+
+def patt(n):
+  for i in range(n):
+    for j in range (i):
+      print("*",end ="")
+    print()
+
+patt(n)
